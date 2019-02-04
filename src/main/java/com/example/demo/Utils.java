e2570e19aeb408addd908aafdd78844a3874fcd4
dpsisodia <devendra.sisodia@gmail.com>
2019-02-04 15:17:16 +0100

    Update Utils.java

--- src/main/java/com/example/demo/Utils.java
+++ src/main/java/com/example/demo/Utils.java
@@ -1,15 +1,12 @@
 package com.example.demo;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.zip.ZipOutputStream;
 
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
@@ -24,17 +21,14 @@ import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
 
 public class Utils {
-	private static final Logger log = LoggerFactory.getLogger(Utils .class);
+	private static final Logger log = LoggerFactory.getLogger(Utils.class);
 	private static final String URL_FX_RATE = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref.zip";
 
 	public static File downloadFxRate() {
 		File result = null;
 		try {
 			result = File.createTempFile(LocalDate.now().toString(), ".zip");
-			FileUtils.copyURLToFile(
-					new URL(URL_FX_RATE), 
-					result 
-					);
+			FileUtils.copyURLToFile(new URL(URL_FX_RATE), result);
 			log.debug("downloaded file {}", result);
 			result = unzip(result.getAbsolutePath());
 		} catch (IOException e) {
@@ -42,29 +36,27 @@ public class Utils {
 		}
 		return result;
 	}
-	
-	public static File unzip(String source){
-	    String destPath = "/tmp/" + LocalDate.now().toString();
+
+	public static File unzip(String source) {
+		String destPath = "/tmp/" + LocalDate.now().toString();
 		try {
 			ZipFile zipFile = new ZipFile(source);
-	        	zipFile.extractAll(destPath);
-	    } catch (ZipException | IOException e) {
-	        e.printStackTrace();
-	    }
-	    return new File(destPath).listFiles()[0];	    
+			zipFile.extractAll(destPath);
+		} catch (ZipException e) {
+			e.printStackTrace();
+		}
+		return new File(destPath).listFiles()[0];
 	}
-	
+
 	public static List<Map<String, String>> convert(File file) throws JsonProcessingException, IOException {
-	    List<Map<String, String>> response = new LinkedList<Map<String, String>>();
-	    CsvMapper mapper = new CsvMapper();
-	    CsvSchema schema = CsvSchema.emptySchema().withHeader();
-	    MappingIterator<Map<String, String>> iterator = mapper.reader(Map.class)
-	            .with(schema)
-	            .readValues(file);
-	    while (iterator.hasNext()) {
-	        response.add(iterator.next());
-	    }
-	    return response;
-	}	
+		List<Map<String, String>> response = new LinkedList<Map<String, String>>();
+		CsvMapper mapper = new CsvMapper();
+		CsvSchema schema = CsvSchema.emptySchema().withHeader();
+		MappingIterator<Map<String, String>> iterator = mapper.reader(Map.class).with(schema).readValues(file);
+		while (iterator.hasNext()) {
+			response.add(iterator.next());
+		}
+		return response;
+	}
 
 }
